1992542e6d58cc3354cbc4576301e0c70ddd1846
Vishal Pandey <vishalpandeyviptsk@gmail.com>
2020-08-17 15:24:58 +0530

    Update urls.py

--- face/urls.py
+++ face/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('status.urls')),
-    path('comments/', include('django_comments.urls'))
+    path('comments/', include('django_comments.urls')),
+    path('create/',include('ckeditor_uploader.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL,
                               document_root=settings.MEDIA_ROOT)
